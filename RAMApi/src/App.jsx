f9fc59671983ab2d0d7102a98a63a226b4dffafa
Fatma Beyzanur TOLAN <fbt.beyza@gmail.com>
2024-07-12 02:06:33 +0300

    create header

---
+++ RAMApi/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap"
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className="App">
+
+        <h1 className='text-center my-4'>
+          <span className='text-primary'> Rick & Morty </span>
+        </h1>
+
+      </div>
+    </>
+  )
+}
+
+export default App
