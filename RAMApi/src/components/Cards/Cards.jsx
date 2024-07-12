5640465558676fbbb99fbca3457dea0a7be33ddf
Fatma Beyzanur TOLAN <fbt.beyza@gmail.com>
2024-07-12 15:50:45 +0300

    done cards components

--- RAMApi/src/components/Cards/Cards.jsx
+++ RAMApi/src/components/Cards/Cards.jsx
@@ -1,4 +1,6 @@
-// import React from 'react'
+import React from 'react'
+import styles from './Cards.module.scss'
+
 
 function Cards({ results }) {
 
@@ -7,14 +9,43 @@ function Cards({ results }) {
 
     if (results) {
         display = results.map(x => {
-            let { id, name, image } = x
+            let { id, name, image, location, status } = x
+
             return (
-                <div key={id} className="col-4">
+                <div key={id} className="col-4 mb-4 position-relative">
                     {/* {name} */}
-                    <div className="">
-                        <img src={image} alt="" className="img-thumbnail" />
+                    <div className={styles.cards}>
+                        <img src={image} alt="" className={`${styles.img}  img-thumbnail`} />
+                        <div style={{ padding: "10px" }} className="content">
+                            <div className="fs-4 fw-bold mb-4"> {name}</div>
+                            <div className="">
+                                <div className="fs-6">Last Lcocation</div>
+                                <div className="fs-5">{location.name}  </div>
+                            </div>
+                        </div>
                     </div>
-                </div>)
+                    {(() => {
+                        if (status === "Dead") {
+                            return (
+                                <div className={`${styles.badge}  position-absolute badge text-bg-danger`} > {status} </div>
+                            )
+                        } else if (status === "Alive") {
+                            return (
+                                <div className={`${styles.badge}  position-absolute badge text-bg-success`} > {status} </div>
+                            )
+                        } else if (status === "unknown") {
+                            return (
+                                <div className={`${styles.badge}  position-absolute badge text-bg-secondary`} > {status} </div>
+                            )
+                        } else {
+                            return (
+                                <div className={`${styles.badge}  position-absolute badge text-bg-dark`} > --- </div>
+                            )
+                        }
+                    })()}
+                    {/* <div className={`${styles.badge}  position-absolute badge text-bg-danger`} > {status} </div> */}
+                </div >
+            );
         });
     }
     else { display = "No Character Found:/" }
