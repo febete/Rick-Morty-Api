a8e4d834e150223503e257064af8070e996015b0
Fatma Beyzanur TOLAN <fbt.beyza@gmail.com>
2024-07-13 12:39:24 +0300

    done episodes

--- RAMApi/src/Pages/Episodes.jsx
+++ RAMApi/src/Pages/Episodes.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Cards from '../components/Cards/Cards'
+import InputGroup from '../components/Filters/Category/InputGroup';
 
 function Episodes() {
 
@@ -42,7 +43,11 @@ function Episodes() {
 
             <div className="row">
                 <div className="col-3">
-                    Pick Episodes
+                    <h4 className="text-center mb-4">
+                        Pick Episodes
+                    </h4>
+                    <InputGroup setID={setID} name="Episode" total={51} />
+
                 </div>
 
                 <div className="col-8">
---
+++ RAMApi/src/components/Filters/Category/InputGroup.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function InputGroup({ total, name, setID }) {
+    console.log([...Array(total).keys()])
+    return (
+        <div class="input-group mb-3">
+            <label class="input-group-text" for="inputGroupSelect01">Options</label>
+            <select
+                onChange={e => setID(e.target.value)}
+                class="form-select" id={name}>
+                <option selected>Choose...</option>
+
+                {[...Array(total).keys()].map(x => {
+                    return (
+                        <option value={x + 1}>
+                            {name} - {x + 1}
+                        </option>
+                    )
+                })}
+
+            </select>
+        </div>
+
+    )
+}
+
+export default InputGroup
