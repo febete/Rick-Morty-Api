02e36a88b9d531fc8910c7554c7dbe3e84263416
Fatma Beyzanur TOLAN <fbt.beyza@gmail.com>
2024-07-13 03:36:06 +0300

    special filter button

--- RAMApi/src/components/Filters/FilterBTN.jsx
+++ RAMApi/src/components/Filters/FilterBTN.jsx
@@ -3,9 +3,25 @@ import React from 'react'
 function FilterBTN({ name, index, items }) {
     return (
         <>
+            {/* Hide the radio button */}
+            {/* Whenever our input is checked */}
+            <style jsx>
+                {`               
+                .x:checked + label{
+                    background-color: #0b5ed7;
+                    color:white;
+                }
+
+                    input[type = "radio"]{
+                    display:none;
+                    }
+                `}
+
+            </style>
+
             <div className="form-check">
                 <input
-                    className="form-check-input" type="radio"
+                    className="form-check-input x" type="radio"
                     name={name}
                     id={`${name} - ${index}`}
 
